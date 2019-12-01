0fb5d7c79171489fa9c1cb159dfb505191ac581f
marvin lara <laramarvin105@gmail.com>
2019-12-01 17:59:35 -0500

    Updates

---
+++ precache-manifest.e01c240983892246c4180ed4630694c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692e5e224504105b5669b44f676cd73b",
+    "url": "/googlebooks/index.html"
+  },
+  {
+    "revision": "9953dc7a92e3cac76ad9",
+    "url": "/googlebooks/static/css/main.0a49dd09.chunk.css"
+  },
+  {
+    "revision": "3fe6c18207d4b05b3b54",
+    "url": "/googlebooks/static/js/2.3d364876.chunk.js"
+  },
+  {
+    "revision": "9953dc7a92e3cac76ad9",
+    "url": "/googlebooks/static/js/main.6182e118.chunk.js"
+  },
+  {
+    "revision": "4a08c65bbec0b6ba5a19",
+    "url": "/googlebooks/static/js/runtime-main.04e164b4.js"
+  }
+]);
